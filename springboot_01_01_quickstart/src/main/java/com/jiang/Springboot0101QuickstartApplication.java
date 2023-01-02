bc99504e9add54077a1694b238cee79491969aaa
Jiangronghua <613870505@qq.com>
2023-01-02 15:11:34 +0800

    快速创建工程
    通过模版项目快速创建工程

--- springboot_01_01_quickstart/src/main/java/com/jiang/Springboot0101QuickstartApplication.java
+++ springboot_01_01_quickstart/src/main/java/com/jiang/Springboot0101QuickstartApplication.java
@@ -1,13 +1,19 @@
 package com.jiang;
 
+import com.jiang.controller.BookController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Springboot0101QuickstartApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springboot0101QuickstartApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(Springboot0101QuickstartApplication.class, args);
+        BookController bean = ctx.getBean(BookController.class);
+        User user = ctx.getBean(User.class);
+        System.out.println("bean=======>" + bean);
+        System.out.println("user=======>" + user);
     }
 
 }
---
+++ springboot_01_01_quickstart/src/main/java/com/jiang/User.java
@@ -0,0 +1,10 @@
+package com.jiang;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jiangronghua
+ */
+@RestController
+public class User {
+}
