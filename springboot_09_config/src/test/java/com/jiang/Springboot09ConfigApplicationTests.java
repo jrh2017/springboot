365ba9b7d2d217a4d33a805dfc6c59ed3aeb397a
Jiangronghua <613870505@qq.com>
2023-01-06 12:49:23 +0800

    springboot四级配置

---
+++ springboot_09_config/src/test/java/com/jiang/Springboot09ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot09ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
