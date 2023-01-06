2f472ad50eacfce1267ccf3094af79ff0ea8c400
Jiangronghua <613870505@qq.com>
2023-01-06 13:32:07 +0800

    springboot多环境配置
    springboot多环境配置

---
+++ springboot_10_profiles/src/test/java/com/jiang/Springboot10ProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot10ProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
