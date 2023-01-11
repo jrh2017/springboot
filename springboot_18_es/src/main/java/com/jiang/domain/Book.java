b533c616ba9b94ed0f849891f0ee4ff5f6cf6a97
Jiangronghua <613870505@qq.com>
2023-01-11 22:21:02 +0800

    springboot 整合es搜索完结
    springboot 整合es搜索完结

--- springboot_18_es/src/main/java/com/jiang/domain/Book.java
+++ springboot_18_es/src/main/java/com/jiang/domain/Book.java
@@ -7,7 +7,7 @@ import lombok.Data;
  */
 @Data
 public class Book {
-    private int id;
+    private Integer id;
     private String name;
     private String type;
     private String description;
--- springboot_18_es/src/test/java/com/jiang/Springboot18EsApplicationTests.java
+++ springboot_18_es/src/test/java/com/jiang/Springboot18EsApplicationTests.java
@@ -1,12 +1,26 @@
 package com.jiang;
 
+import com.alibaba.fastjson.JSON;
 import com.jiang.dao.BookDao;
+import com.jiang.domain.Book;
 import org.apache.http.HttpHost;
-import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +28,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.util.List;
 
 @SpringBootTest
 class Springboot18EsApplicationTests {
@@ -50,6 +65,85 @@ class Springboot18EsApplicationTests {
     void testCreateIndex() throws IOException {
         CreateIndexRequest request = new CreateIndexRequest("books");
         client.indices().create(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testCreateIndexByIK() throws IOException {
+        CreateIndexRequest request = new CreateIndexRequest("books");
+        String json = "{\n" +
+                "    \"mappings\": {\n" +
+                "        \"properties\": {\n" +
+                "            \"id\": {\n" +
+                "                \"type\": \"keyword\"\n" +
+                "            },\n" +
+                "            \"name\": {\n" +
+                "                \"type\": \"text\",\n" +
+                "                \"analyzer\": \"ik_max_word\",\n" +
+                "                \"copy_to\": \"all\"\n" +
+                "            },\n" +
+                "            \"type\": {\n" +
+                "                \"type\": \"keyword\"\n" +
+                "            },\n" +
+                "            \"description\": {\n" +
+                "                \"type\": \"text\",\n" +
+                "                \"analyzer\": \"ik_max_word\",\n" +
+                "                \"copy_to\": \"all\"\n" +
+                "            },\n" +
+                "            \"all\": {\n" +
+                "                \"type\": \"text\",\n" +
+                "                \"analyzer\": \"ik_max_word\"\n" +
+                "            }\n" +
+                "        }\n" +
+                "    }\n" +
+                "}";
+        request.source(json, XContentType.JSON);
+        client.indices().create(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testCreateDoc() throws IOException {
+        Book book = bookDao.selectById("1");
+        IndexRequest request = new IndexRequest("books").id(book.getId().toString());
+        String json = JSON.toJSONString(book);
+        request.source(json, XContentType.JSON);
+        client.index(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testCreateDocAll() throws IOException {
+        List<Book> bookList = bookDao.selectList(null);
+        BulkRequest bulk = new BulkRequest();
+        for (Book book: bookList) {
+            IndexRequest request = new IndexRequest("books").id(book.getId().toString());
+            String json = JSON.toJSONString(book);
+            request.source(json, XContentType.JSON);
+            bulk.add(request);
+        }
+        client.bulk(bulk, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testGet() throws IOException {
+
+        GetRequest request = new GetRequest("books", "1");
+        GetResponse response = client.get(request, RequestOptions.DEFAULT);
+        String json = response.getSourceAsString();
+        System.out.println(json);
+    }
+    
+    @Test
+    void testSearch() throws IOException {
+        SearchRequest request = new SearchRequest("books");
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        builder.query(QueryBuilders.termQuery("all", "spring"));
+        request.source(builder);
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        SearchHits hits = response.getHits();
+        for (SearchHit hit : hits) {
+            String source = hit.getSourceAsString();
+            JSON.parseObject(source, Book.class);
+            System.out.println(source);
+        }
     }
 
 }
