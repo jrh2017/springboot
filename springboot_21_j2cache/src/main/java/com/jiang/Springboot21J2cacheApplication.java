bc39f1f647c9be54882bd3120c7494c26010b19a
Jiangronghua <613870505@qq.com>
2023-01-14 14:54:26 +0800

    springboot整合j2cache
    springboot整合j2cache

---
+++ springboot_21_j2cache/src/main/java/com/jiang/Springboot21J2cacheApplication.java
@@ -0,0 +1,13 @@
+package com.jiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot21J2cacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot21J2cacheApplication.class, args);
+    }
+
+}
